3c46d7b499f4d4e266a214bc9b2370c19e7afc80
Oleksiy Blavat <oblavat@invoca.com>
2022-12-27 15:57:02 -0500

    linked styles and basic scripts to thymeleaf - primefaces integration was terrible - don't attempt in the future, stick to barebones

--- src/main/java/com/application/rezz/RezzApplication.java
+++ src/main/java/com/application/rezz/RezzApplication.java
@@ -1,11 +1,13 @@
 package com.application.rezz;
 
+
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+
 
 @SpringBootApplication
 @Log4j2
